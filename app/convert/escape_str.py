99aa942642ad2cbf09b8d3869df4666392b79b21
yuyosy <y-ysss@outlook.com>
2022-03-31 23:57:19 +0900

    Add(app/convert/escape_str.py): add unescape_str function

--- app/convert/escape_str.py
+++ app/convert/escape_str.py
@@ -2,3 +2,8 @@ def escape_str(string: str) -> str:
     if not isinstance(string, str):
         string = str(string)
     return string.replace('.', '\\.')
+
+def unescape_str(string: str) -> str:
+    if not isinstance(string, str):
+        string = str(string)
+    return string.replace('\\.', '.')
