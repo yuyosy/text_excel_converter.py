e3b8e5e7e774736139b79a4888520b951d321b53
yuyosy <y-ysss@outlook.com>
2022-04-03 17:32:25 +0900

    Update(app/convert/set_metadata.py): update set_metadata method

--- app/convert/set_metadata.py
+++ app/convert/set_metadata.py
@@ -1,19 +1,40 @@
-from dataclasses import dataclass
 from datetime import datetime
-from typing import Any, Dict
+from pathlib import Path
 from uuid import uuid4
 
 from dataobject.data_object import DataObject
+from ulid.ulid import ULID
+from util.version_value import VersionValue
 
-from .class_definitions import Definitions
+from .class_definitions import DefinitionsData
 from .class_metadata import Metadata
+from .get_hash import get_json_hash
 
-DATETIME_FORMAT = '%Y%m%d-%H%M%S'
 
-def set_metadata(definitions: Definitions, data:DataObject) -> Metadata:
+def set_metadata(data: DataObject, definitions_data: DefinitionsData, filename:Path) -> None:
     metadata = Metadata()
-    
-    return metadata
+    now = datetime.now()
+    metadata.data_id = str(id) if (id := data.get('$metadata.data_id')) else str(uuid4())
+    metadata.chronological_id_current = ULID()
+    metadata.chronological_id_previous = ULID.from_uuid_str(id) if (id := data.get('$metadata.chronological_id_current')) else None
 
-def get_metadata():
-    pass
+    metadata.datetime_created = dt if isinstance(dt := data.get('$metadata.datetime_created'), datetime) else now
+    metadata.datetime_modified = dt if isinstance(dt := data.get('$metadata.datetime_modified'), datetime) else now
+    metadata.datetime_generated_excel = dt if isinstance(dt := data.get('$metadata.datetime_generated_excel'), datetime) else None
+    metadata.datetime_generated_text = now
+
+    metadata.name = definitions_data.name
+    metadata.format_version = VersionValue(definitions_data.format_version)
+    metadata.definitions_file = definitions_data.file_name
+    metadata.source_data = filename
+    metadata.data_version = VersionValue(str(ver)) if (ver := data.get('$metadata.data_version')) else VersionValue('0')
+    attrdict = data.asattrdict()
+    attrdict.pop('$metadata', None)
+    metadata.data_hash_current = get_json_hash(attrdict)
+    metadata.data_hash_previous = str(hs) if (hs := data.get('$metadata.data_hash_current')) else None
+    if metadata.data_hash_current != metadata.data_hash_previous:
+        metadata.datetime_modified = now
+        metadata.data_version.increment('1')
+
+
+    data.update({'$metadata': metadata.todict()})
