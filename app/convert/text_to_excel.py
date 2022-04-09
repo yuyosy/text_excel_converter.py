e88fed9d560baf9c41f2682c9363f37786bef5b2
yuyosy <y-ysss@outlook.com>
2022-04-09 22:32:39 +0900

    Update(app/convert/text_to_excel.py, app/convert/write_*.py): update

--- app/convert/text_to_excel.py
+++ app/convert/text_to_excel.py
@@ -1,5 +1,48 @@
+from logging import getLogger
+from pathlib import Path
+from typing import Any, Callable, Dict
+
+from config.config_class import ConfigApp
+from openpyxl.workbook.workbook import Workbook
+
+from .check_version import check_datafile_version
+from .class_definitions import Definitions
+from .converter import Converter
+from .load_definitions import load_definisions
+from .set_metadata import set_metadata
+from .write_keyvalue import write_keyvalue
+from .write_table import write_table
+
+applogger = getLogger('app')
+filelogger = getLogger('file')
+
+
 
 from .converter import Converter
 
+
 class TextToExcel(Converter):
-    pass
+    def __init__(self, config: ConfigApp) -> None:
+        super().__init__(config)
+        
+        self.read_action: Dict[str, Callable] = {
+            'key-value': write_keyvalue,
+            'table': write_table
+        }
+
+
+    def read(self, datadict:Dict[str, Any], *, filename:Path) -> None:
+        applogger.info('@read')
+        self.data.update(datadict)
+        current, latest = check_datafile_version(datadict.get('$metadata', {}), self.config.options.definitions)
+        applogger.info('@checked workbook version')
+        applogger.info('CurrentVersion: %s', current)
+        applogger.info('LatestVersion: %s', latest)
+        if not current:
+            raise Exception
+        self.definition_data = load_definisions(current)
+        filelogger.debug(self.definition_data)
+
+
+    def write(self, path:Path) -> None:
+        applogger.info('@write')
---
+++ app/convert/write_keyvalue.py
@@ -0,0 +1,2 @@
+def write_keyvalue():
+    pass
