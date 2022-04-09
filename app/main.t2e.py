a9b968885595eca4af8e9add1b80c61c225fb18e
yuyosy <y-ysss@outlook.com>
2022-04-09 22:33:11 +0900

    Update(app/main.t2e.py): update

--- app/main.t2e.py
+++ app/main.t2e.py
@@ -1,14 +1,14 @@
+from fileinput import filename
 import os
+import sys
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from logging import getLogger
-import sys
-
-from openpyxl import load_workbook
 
 from applogging.logger import default_logger_config, set_logger_config
 from config.config import init_config
-from convert.text_to_excel import TextToExcel
 from convert.exceptions import DefinisionsFileException
+from convert.load_datafile import load_textfile
+from convert.text_to_excel import TextToExcel
 from util.resource_path import resource_path
 
 
@@ -28,3 +28,29 @@ if __name__ == '__main__':
 
     parser = set_parser()
     args = parser.parse_args()
+
+    try:
+        config = init_config(args.config, 'utf-8')
+        set_logger_config(config.logging)
+        # file = input('Input>>') if args.input is None else args.input
+        file = resource_path('data/input/1.0.json')  # -------------DEBUG
+
+        appconfig = config.presets.get(args.mode)
+        if not appconfig:
+            raise Exception
+
+        datadict = load_textfile(file)
+        print(datadict)
+
+        converter = TextToExcel(appconfig)
+        converter.read(datadict, filename=file)
+        converter.write(resource_path('data/output/1.0.xlsx'))
+
+    except DefinisionsFileException as err:
+        applogger.exception(err)
+        exit_code = 21
+    except Exception as err:
+        applogger.exception(err)
+        exit_code = 1
+    applogger.info('Finished')
+    sys.exit(exit_code)
