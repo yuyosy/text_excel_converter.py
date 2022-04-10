5b29955357e59f7a3370d8de68900b62b13c21c7
yuyosy <y-ysss@outlook.com>
2022-04-10 20:39:33 +0900

    Update(app/convert/write_*.py): implement write keyvalue, table functions

--- app/convert/write_keyvalue.py
+++ app/convert/write_keyvalue.py
@@ -1,2 +1,41 @@
-def write_keyvalue():
-    pass
+
+from typing import Any, Iterator, Tuple
+
+from dataobject.data_object import DataObject
+from openpyxl.cell import Cell
+from openpyxl.workbook.workbook import Workbook
+
+from .class_definitions import Definitions
+from .escape_str import escape_str
+from .marshal_data_format import FormatType, unmarshalling
+from .util_convert import get_key_list
+from .util_sheet_index import (to_columns_indexes_tuple,
+                               to_relative_column_index)
+
+
+def write_keyvalue(workbook: Workbook, data: DataObject, definitions: Definitions) -> Iterator[Tuple[Cell, Any]]:
+    sheet = workbook[definitions.sheet]
+    key_list = get_key_list(definitions.data)
+
+    min_col, max_col = to_columns_indexes_tuple(definitions.columns)
+    key_col = to_relative_column_index(0 if (c := definitions.key_col) is None else c, min_col)
+    val_col = to_relative_column_index(0 if (c := definitions.value_col) is None else c, min_col)
+    header_row = 0 if (c := definitions.header_row) is None else c
+    begin_row = 0 if (c := definitions.begin_row) is None else c
+
+    for row in sheet.iter_rows(min_row=begin_row, min_col=min_col, max_col=max_col):
+        key_cell = row[key_col]
+        val_cell = row[val_col]
+        if key_cell.value is None:
+            break
+        key = key_list.get(key_cell.value, None)
+        if key is None:
+            continue
+        k = f'{parent}.{escape_str(key)}' if (parent := definitions.parent) else escape_str(key)
+        value = unmarshalling(data.get(k, None), definitions.data.get(key))
+        if value is FormatType.PASS:
+            continue
+        # val_cell.value = value
+        yield val_cell, value
+        if key_cell.value == definitions.endkeyif:
+            break
--- app/convert/write_table.py
+++ app/convert/write_table.py
@@ -1,2 +1,54 @@
-def write_table():
-    pass
+
+from typing import Any, Iterator, List, Tuple
+
+from dataobject.data_object import DataObject
+from openpyxl.cell import Cell
+from openpyxl.workbook.workbook import Workbook
+
+from .class_definitions import Definitions
+from .escape_str import escape_str
+from .marshal_data_format import FormatType, unmarshalling
+from .util_convert import get_key_list
+from .util_sheet_index import (to_columns_indexes_tuple,
+                               to_relative_column_index)
+
+
+def write_table(workbook: Workbook, data: DataObject, definitions: Definitions) -> Iterator[Tuple[Cell, Any]]:
+    sheet = workbook[definitions.sheet]
+    key_list = get_key_list(definitions.data)
+
+    min_col, max_col = to_columns_indexes_tuple(definitions.columns)
+    key_col = to_relative_column_index(0 if (c := definitions.key_col) is None else c, min_col)
+    val_col = to_relative_column_index(0 if (c := definitions.value_col) is None else c, min_col)
+    header_row = 0 if (c := definitions.header_row) is None else c
+    begin_row = 0 if (c := definitions.begin_row) is None else c
+
+    header = [col.value for col in sheet[header_row][min_col-1:max_col]]
+    table_data = data.catchup(parent).asattrdict() if (parent := definitions.parent) else data.asattrdict()
+    max_row = begin_row+len(table_data)
+    
+    for data_key, row in zip(table_data, sheet.iter_rows(min_row=begin_row, max_row=max_row, min_col=min_col, max_col=max_col)):
+        key_cell = row[key_col]
+        # if not key_cell.value:
+        #     continue
+        d = table_data.get(escape_str(data_key)) or {}
+        cells_data: List[Tuple[Cell, Any]] = []
+        key_header_value = key_list.get(header[key_col], None)
+        for head, cell in zip(header, row):
+            header_key = key_list.get(head, '')
+            if not header_key:
+                continue
+            if definitions.overwrite_keycol and header_key == key_header_value:
+                cells_data.append((cell, unmarshalling(data_key, definitions.data.get(header_key))))
+            else:
+                cells_data.append((cell, unmarshalling(d.get(header_key), definitions.data.get(header_key))))
+                
+        if key_cell.value == definitions.endkeyif:
+            break
+
+        for k, v in cells_data:
+            if k and v is not FormatType.PASS:
+                # k.value = v
+                yield k, v
+        if key_cell.value == definitions.endkeyif:
+            break
