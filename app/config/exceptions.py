7e1e600d5a763c2f898d42dea34123b8fcef8cf3
yuyosy <y-ysss@outlook.com>
2022-04-12 00:42:33 +0900

    Update(app/convert/exceptions.py, app/config/exceptions.py): add methods

--- app/config/exceptions.py
+++ app/config/exceptions.py
@@ -1,2 +1,7 @@
 class ConfigException(Exception):
-    pass
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
--- app/convert/exceptions.py
+++ app/convert/exceptions.py
@@ -1,5 +1,10 @@
 class ConvertException(Exception):
-    pass
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
 
 class DefinisionsFileException(ConvertException):
     pass
