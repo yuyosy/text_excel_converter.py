a894c47a94613298f30e95dfdb6cfdbf69a286d5
yuyosy <y-ysss@outlook.com>
2022-04-20 00:09:37 +0900

    Remove: app/convert/excel_to_json.py

--- app/convert/excel_to_json.py
+++
@@ -1,9 +0,0 @@
-# import json
-# from pathlib import Path
-
-
-# class ToJson():
-#     def write_json(self, path: Path, *, encoding='utf-8'):
-#         path.parent.mkdir(parents=True, exist_ok=True)
-#         with path.open('w', encoding=encoding) as f:
-#             f.write(json.dumps(self.data.asattrdict(), indent=4, ensure_ascii=False, default=str))
