8f462058f4de37b3e02801b56e280c7ee61a9e1f
yuyosy <y-ysss@outlook.com>
2022-05-27 23:46:09 +0900

    Update(app/convert/util_convert.py): add is_overwrite function

--- app/convert/util_convert.py
+++ app/convert/util_convert.py
@@ -1,4 +1,7 @@
-from typing import Any, Dict, List
+import re
+from typing import Any, Dict
+
+from dataobject.data_object import DataObject
 
 
 def get_key(format: Dict[str, Any]) -> str:
@@ -11,3 +14,16 @@ def get_key(format: Dict[str, Any]) -> str:
 
 def get_key_list(data: Dict[str, Any]) -> Dict[str, str]:
     return {get_key(data.get(item)): item for item in data}
+
+
+def overwrite(option: Dict[str, Any], value: Any, data: DataObject) -> bool:
+    if isinstance(option, dict) and (opt := option.get('overwrite', None)):
+        mode = opt.get('mode', None)
+        string = opt.get('string', '')
+
+        for param in re.findall(r'@\{.+?\}', string):
+            replaceto = val if (val := data.get(param[2:-1])) else ''
+            string = string.replace(param, replaceto)
+        if (mode == 'not-eq' and string == value) or (mode == 'eq' and string != value):
+            return False
+    return True
